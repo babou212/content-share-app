18cde095079264c98df3233222f8fcab498dfa46
Dylan Cree <dylanpab1@gmail.com>
2024-11-20 22:09:49 +0000

    added azure app insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import NavBar from "~/components/navbar";
 
+import AzureAppInsights from "..//components/azureApplicationInsights";
+
 export const metadata: Metadata = {
   title: "Content Share Application",
   description: "Content Share website for sharing blog content",
@@ -16,10 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>
+      <AzureAppInsights>
        <div className="overflow-auto w-full">
           <NavBar />
           <main className="min-h-screen bg-gray-100 dark:bg-gray-900">{children}</main>
         </div>
+        </AzureAppInsights>
       </body>
     </html>
   );
