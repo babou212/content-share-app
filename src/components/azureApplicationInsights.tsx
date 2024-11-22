8f9f6fa5733d43ac39f2c20856bbbe4164a38ce2
Dylan Cree <dylanpab1@gmail.com>
2024-11-22 00:36:14 +0000

    refactored for application insights

--- src/components/azureApplicationInsights.tsx
+++ src/components/azureApplicationInsights.tsx
@@ -1,34 +1,34 @@
-// /* eslint-disable @typescript-eslint/consistent-type-imports */
-// "use client";
+/* eslint-disable @typescript-eslint/consistent-type-imports */
+"use client";
 
-// import { ReactNode, useEffect } from "react";
-// import { ApplicationInsights } from "@microsoft/applicationinsights-web";
-// import { ReactPlugin, AppInsightsContext } from "@microsoft/applicationinsights-react-js";
+import { ReactNode, useEffect } from "react";
+import { ApplicationInsights } from "@microsoft/applicationinsights-web";
+import { ReactPlugin, AppInsightsContext } from "@microsoft/applicationinsights-react-js";
 
-// export function AppInsightsProvider({ children }: { children: ReactNode }) {
-//   const reactPlugin = new ReactPlugin();
-//   useEffect(() => {
-//     const appInsights = new ApplicationInsights({
-//       config: {
-//         connectionString: process.env.NEXT_PUBLIC_APP_APPLICATIONINSIGHTS_CONNECTION_STRING,
-//         enableAutoRouteTracking: true,
-//         enableCorsCorrelation: true,
-//         enableRequestHeaderTracking: true,
-//         enableResponseHeaderTracking: true,
-//         enableAjaxPerfTracking: true,
-//         isBrowserLinkTrackingEnabled: true,
-//         extensions: [reactPlugin],
-//         extensionConfig: {
-//           [reactPlugin.identifier]: {},
-//         },
-//       },
-//     });
-//     appInsights.loadAppInsights();
+export function AppInsightsProvider({ children }: { children: ReactNode }) {
+  const reactPlugin = new ReactPlugin();
+  useEffect(() => {
+    const appInsights = new ApplicationInsights({
+      config: {
+        connectionString: process.env.NEXT_PUBLIC_APP_APPLICATIONINSIGHTS_CONNECTION_STRING,
+        enableAutoRouteTracking: true,
+        enableCorsCorrelation: true,
+        enableRequestHeaderTracking: true,
+        enableResponseHeaderTracking: true,
+        enableAjaxPerfTracking: true,
+        isBrowserLinkTrackingEnabled: true,
+        extensions: [reactPlugin],
+        extensionConfig: {
+          [reactPlugin.identifier]: {},
+        },
+      },
+    });
+    appInsights.loadAppInsights();
 
-//     return () => {
-//       appInsights.unload();
-//     };
-//   });
+    return () => {
+      appInsights.unload();
+    };
+  });
 
-//   return <AppInsightsContext.Provider value={reactPlugin}>{children}</AppInsightsContext.Provider>;
-// }
+  return <AppInsightsContext.Provider value={reactPlugin}>{children}</AppInsightsContext.Provider>;
+}
