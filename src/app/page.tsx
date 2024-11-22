12bb9825f32e05eb84d1c3dd692e42035b0cdd09
Dylan Cree <dylanpab1@gmail.com>
2024-11-22 23:12:41 +0000

    fixed navbar covering top cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export const dynamic = 'force-dynamic';
 
 export default async function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center text-white bg-slate-100">
-      <div className="container flex flex-col items-center gap-12 px-4 py-16">
+    <main className="flex min-h-screen flex-col items-center justify-center mt-8 text-white bg-slate-100">
+      <div className="container flex flex-col items-center mt-8 gap-12 px-4 py-16">
         <Docs />
       </div>
     </main>
